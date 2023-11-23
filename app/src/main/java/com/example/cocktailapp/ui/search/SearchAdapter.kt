8dfa0d9fe5f170536865486668a00218e6076b93
tgaignard001 <tgaignard001@bordeaux-inp.fr>
2023-11-23 18:28:59 +0100

    search fragment working with loader and no result

--- app/src/main/java/com/example/cocktailapp/ui/search/SearchAdapter.kt
+++ app/src/main/java/com/example/cocktailapp/ui/search/SearchAdapter.kt
@@ -1,12 +1,19 @@
 package com.example.cocktailapp.ui.search
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.cocktailapp.core.model.Drink
+import com.example.cocktailapp.core.model.DrinksResponse
 import com.example.cocktailapp.databinding.SearchItemBinding
+import com.squareup.picasso.Picasso
 
-class SearchAdapter(private val nameList: List<String>)
-    : RecyclerView.Adapter<SearchAdapter.SearchViewHolder>() {
+class SearchAdapter(private val cocktailList: DrinksResponse) :
+    RecyclerView.Adapter<SearchViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SearchViewHolder {
         val binding = SearchItemBinding
@@ -14,17 +21,22 @@ class SearchAdapter(private val nameList: List<String>)
         return SearchViewHolder(binding)
     }
 
-    override fun getItemCount() = nameList.size
+    override fun getItemCount() = cocktailList.drinks?.count() ?: 0
 
     override fun onBindViewHolder(holder: SearchViewHolder, position: Int) {
-        with(holder){
-            with(nameList[position]) {
-                binding.searchNameTextView.text = this
-            }
+        val drink: Drink? = cocktailList.drinks?.get(position)
+        holder.titleTextView.text = drink?.title ?: ""
+        Picasso.get().load(drink?.imageURL).into(holder.imageView)
+        holder.cardContainer.setOnClickListener {
+            Log.d("CARD", "Cocktail ${drink?.title} clicked" )
         }
     }
 
-    inner class SearchViewHolder(val binding: SearchItemBinding)
-        :RecyclerView.ViewHolder(binding.root)
 
+}
+
+class SearchViewHolder(val binding: SearchItemBinding) : RecyclerView.ViewHolder(binding.root) {
+    val titleTextView: TextView = binding.cocktailName
+    val imageView: ImageView = binding.cocktailImage
+    val cardContainer: CardView = binding.searchCard
 }
